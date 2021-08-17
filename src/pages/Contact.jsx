03367dda91be70e2c0265dd121db311f72062423
Hoan <nvhoan911@gmail.com>
2021-08-17 11:37:47 +0700

    no message

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,11 +5,7 @@ import Helmet from '../components/Helmet'
  const Contact = () => {
     return (
         <Helmet title="Contact">
-            <div className="container">
-                <div className="main">
-                    Contact
-                </div>
-            </div>
+            Contact
         </Helmet>
     )
 }
