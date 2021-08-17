e2f0bc648630976bea1907d584de82f445614651
Hoan <nvhoan911@gmail.com>
2021-08-17 11:40:23 +0700

    a

--- src/components/AccountBox/LoginForm.jsx
+++ src/components/AccountBox/LoginForm.jsx
@@ -1,23 +1,35 @@
-import React, {useContext} from 'react'
-import { BoldLink, BoxContainer, FormContainer, Input, MutedLink, SubmitButton } from './common'
-import { Marginer } from "../Marginer"
-import { AccountContext } from './accountContext'
-export const LoginForm = (props) => {
-const { switchToSignup } = useContext(AccountContext);
+import React, { useContext, useState } from 'react';
+import { BoldLink, BoxContainer, FormContainer, Input, MutedLink, SubmitButton, LogErrors } from './common'
+import { Marginer } from "../Marginer";
+import { AccountContext } from './accountContext';
+import validateAccount from "../Auth-Login/validateAccount"
+import Login from "../Auth-Login/Login"
 
+
+export const LoginForm = () => {
+    const { switchToSignup, switchToForgot } = useContext(AccountContext);
+    const { handleChange, values, handleSubmit, errors } = Login(validateAccount);
     return (
-        <BoxContainer>
-            <FormContainer>
-                <Input type="email" placeholder="Email" />
-                <Input type="password" placeholder="Password" />
+        <>
+            <form onSubmit={handleSubmit}>
+                {/* {(error !== "") ? (<div>{error}</div>) : ""} */}
+                <BoxContainer >
 
-            </FormContainer>
-            <Marginer direction="vetical" margin={10} />
-            <MutedLink href="#">Forget your password?</MutedLink>
-            <Marginer direction="vetical" margin="1.6em" />
-            <SubmitButton type="submit">SignIn</SubmitButton>
-            <Marginer direction="vetical" margin="1em" />
-            <MutedLink href="#">Don't have an account?<BoldLink href="#" onClick={switchToSignup}>SignUp</BoldLink></MutedLink>
-        </BoxContainer>
+                    <FormContainer>
+                        <Input type="email" placeholder="Email" name="email" value={values.email} onChange={handleChange} />
+                        {errors.password && <LogErrors>{errors.email}</LogErrors>}
+                        <Input type="password" placeholder="Password" name="password" value={values.password} onChange={handleChange} />
+                        {errors.password && <LogErrors>{errors.password}</LogErrors>}
+                    </FormContainer>
+                    <Marginer direction="vetical" margin={10} />
+                    <MutedLink href="#" onClick={switchToForgot}>Forget your password?</MutedLink>
+                    <Marginer direction="vetical" margin="1.6em" />
+                    <SubmitButton type="submit">SignIn</SubmitButton>
+                    <Marginer direction="vetical" margin="1em" />
+                    <MutedLink href="#">Don't have an account?<BoldLink href="#" onClick={switchToSignup}>SignUp</BoldLink></MutedLink>
+
+                </BoxContainer>
+            </form>
+        </>
     )
 }
