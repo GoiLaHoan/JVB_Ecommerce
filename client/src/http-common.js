c93a9114d25ac027f3a8b0ad2c17737b713b607f
Nguyễn Đức Dũng <nguyenducdung853@gmail.com>
2021-09-13 14:22:39 +0700

    final commit DungRoman

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: "https://jvb-ecommerce.herokuapp.com/api",
   headers: {
     "Content-Type": "application/json",
     
